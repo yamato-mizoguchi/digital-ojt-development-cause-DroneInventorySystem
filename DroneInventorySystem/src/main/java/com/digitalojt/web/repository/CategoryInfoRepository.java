46b64fde52126afd19da9e3a3d75d87585c85788
z2240246 <z2240246@N2311342>
2024-10-31 09:14:50 +0900

    分類情報管理：DB操作2

--- DroneInventorySystem/src/main/java/com/digitalojt/web/repository/CategoryInfoRepository.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/repository/CategoryInfoRepository.java
@@ -1,7 +1,11 @@
 package com.digitalojt.web.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.digitalojt.web.entity.CategoryInfo;
@@ -14,5 +18,12 @@ import com.digitalojt.web.entity.CategoryInfo;
  */
 @Repository
 public interface CategoryInfoRepository extends JpaRepository<CategoryInfo, Integer>{
-
+	/**
+	 * 引数に合致する分類情報を取得
+	 * 
+	 * @param categoryName
+	 * @return paramで検索した結果
+	 */
+	@Query("SELECT c FROM CategoryInfo c WHERE c.categoryName = :categoryName")
+    List<CategoryInfo> findByCategoryName(@Param("categoryName") String categoryName);
 }

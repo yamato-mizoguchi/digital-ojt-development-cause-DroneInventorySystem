d03579b9409134a242d69363dffde39b83de63eb
z2240246 <z2240246@N2311342>
2024-11-12 13:17:38 +0900

    認証機能

--- DroneInventorySystem/src/main/java/com/digitalojt/web/config/WebSecurityConfig.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/config/WebSecurityConfig.java
@@ -2,9 +2,17 @@ package com.digitalojt.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.digitalojt.web.consts.UrlConsts;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * WebSecurityConfig
@@ -14,20 +22,60 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
-
+	
+	/* パスワードエンコーダー */
+	private final PasswordEncoder passwordEncoder;
+	
+	/* ユーザー情報取得サービス */
+	private final UserDetailsService userDetailsService;
+	
+	/* 管理者IDのname属性 */
+	private final String ADMIN_ID = "adminId";
+	
+	/* パスワードのname属性 */
+	private final String PASSWORD = "password";
+	
 	/**
+	 * Spring Securityカスタマイズ用
 	 * 
 	 * @param http
-	 * @return
+	 * @return カスタマイズ結果
 	 * @throws Exception
 	 */
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
-		// LoginController呼び出し
-		http.formLogin(login -> login.loginPage("/login/").usernameParameter("adminId"));
+
+	http
+		.authorizeHttpRequests(authorize -> authorize
+			.requestMatchers(UrlConsts.NO_AUTHENTICATION).permitAll()
+			.requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+			.anyRequest().authenticated())
+		.formLogin(login -> login
+			.loginPage(UrlConsts.LOGIN)
+			.loginProcessingUrl(UrlConsts.AUTHENTICATE)
+			.defaultSuccessUrl(UrlConsts.STOCK_LIST)
+			.failureUrl(UrlConsts.LOGIN + "?error=true")
+			.usernameParameter(ADMIN_ID)
+			.passwordParameter(PASSWORD))
+		.logout(logout -> logout.logoutSuccessUrl(UrlConsts.LOGIN));
 		
 		return http.build();
+	}
+	
+	
+	/**
+	 * Provider定義
+	 * 
+	 * @return カスタマイズProvider情報
+	 */
+	@Bean
+	AuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		
+		return provider;
 	}
 }
--- DroneInventorySystem/src/main/java/com/digitalojt/web/repository/AdminInfoRepository.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/repository/AdminInfoRepository.java
@@ -1,5 +1,7 @@
 package com.digitalojt.web.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +15,5 @@ import com.digitalojt.web.entity.AdminInfo;
  */
 @Repository
 public interface AdminInfoRepository extends JpaRepository<AdminInfo, String> {
-
+	Optional<AdminInfo> findByAdminId(String adminId);
 }

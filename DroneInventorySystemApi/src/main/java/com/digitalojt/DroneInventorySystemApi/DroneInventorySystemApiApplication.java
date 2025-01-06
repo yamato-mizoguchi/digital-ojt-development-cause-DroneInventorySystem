85350a79e6245a5691481a806e0a68fa47f65954
unknown <z2240246@intra.ctc-g.co.jp>
2025-01-06 16:01:55 +0900

    API1

---
+++ DroneInventorySystemApi/src/main/java/com/digitalojt/DroneInventorySystemApi/DroneInventorySystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalojt.DroneInventorySystemApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneInventorySystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneInventorySystemApiApplication.class, args);
+	}
+
+}

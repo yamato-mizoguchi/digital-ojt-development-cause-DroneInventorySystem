ada55ef17f2560ae63b222d7ffd8e0f710a3397e
z2240246 <z2240246@10.87.17.10>
2025-01-07 13:39:53 +0900

    一時的なコミット

---
+++ DroneInventorySystemApi/src/test/java/com/digitalojt/api/DroneInventorySystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalojt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneInventorySystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
